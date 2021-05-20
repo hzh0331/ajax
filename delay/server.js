19c8635c2f8ece743bfaa60e093a56cb3ce03b24
ZihaoHan <972802149@qq.com>
2021-05-19 18:28:34 -0700

    delay

---
+++ delay/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+// app.get('/server', (request, response)=>{
+//     response.setHeader('Access-Control-Allow-Origin', '*')
+//     response.send('Hello AJAX');
+// })
+app.all('/delay', (request, response)=>{
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', '*')
+    setTimeout(()=>{
+        response.send('delay');
+    }, 3000)
+})
+
+app.listen(8000, ()=>{
+    console.log("port 8000 is listening")
+})
